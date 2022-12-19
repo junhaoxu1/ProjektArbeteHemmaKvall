bc7f439198e6a682921eb124023359281170f70d
junhaoxu1 <97689187+junhaoxu1@users.noreply.github.com>
2022-12-19 20:47:21 +0100

    Print data in HTML

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+const showImageEl = document.querySelector("#showImage");
+const someDataEl = document.querySelector("#someData");
+
 const fetchCandyList = async () => {
     const res = await fetch("https://www.bortakvall.se/api/products")
     const data = await res.json()
 
-    console.log(data)
     return data
-}
+};
 
-fetchCandyList()
+fetchCandyList().then(data => {
+    someDataEl.innerHTML = `<p>Show Data: ${data['data'][0]['name']}</p>`;
+    console.log(data['data'][0]['name']);
+
+    showImageEl.src = data['data'][0]['images']['large'] 
+});

30a8770f73d2993174c1a475a7d862ed4cf9b772
junhaoxu1 <97689187+junhaoxu1@users.noreply.github.com>
2022-12-27 11:05:43 +0100

    removed console.log

--- assets/script.js
+++ assets/script.js
@@ -183,7 +183,6 @@ let hiddenCart = true;
 function cartWithProducts() {
         if(hiddenCart === true){
             cart.classList.remove("hide"); // tar bort hide klassen, men varukorg syns i DOM
-            console.log("funkar");
             hiddenCart = false;
         } else {
             cart.classList.add("hide"); // gör att varukorg inte syns i DOM

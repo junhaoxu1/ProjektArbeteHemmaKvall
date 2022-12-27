33b8eb21e8b9773611dd7a16faddb3b09e109567
junhaoxu1 <97689187+junhaoxu1@users.noreply.github.com>
2022-12-27 10:51:47 +0100

    Removed old script

--- assets/api.js
+++ assets/api.js
@@ -1,19 +1,16 @@
 const fetchCandyList = async () => {
     const res = await fetch("https://www.bortakvall.se/api/products");
 
-    if (!res.ok) {
-        throw new Error(`${res.status} ${res.statusText}`);
-    }
-
     const data = await res.json();
 
     return data["data"];
 };
 const getProducts = async () => {
     const response = await fetch('https://www.bortakvall.se/api/products');
+    if (!res.ok) {
+        throw new Error(`${res.status} ${res.statusText}`);
+    }
     const data = await response.json();
-    console.log(data)
-
 
     return data["data"];
 }

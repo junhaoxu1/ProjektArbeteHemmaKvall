1f0984b4f1a59191bfee33657be6af1bc70ca815
Michelle <michelleklara.kg@outlook.com>
2023-01-04 20:09:23 +0100

    empty files

--- assets/api.js
+++
@@ -1,9 +0,0 @@
-const getProducts = async () => {
-    const response = await fetch('https://www.bortakvall.se/api/products');
-    if (!response.ok) {
-        throw new Error(`${response.status} ${response.statusText}`);
-    }
-    const data = await response.json();
-
-    return data["data"];
-}

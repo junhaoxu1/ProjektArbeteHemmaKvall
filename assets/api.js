c7fd6396c7eac2ead6c0a039935480122003454b
Andreas Xu <junhaoxu123@gmail.com>
2023-02-10 10:06:09 +0100

    changed API back to bortakvall

--- assets/api.js
+++ assets/api.js
@@ -11,7 +11,7 @@ const getProducts = async () => {
 const confirmBuy = async (data) => {
     try {
       
-      const response = await fetch('http://localhost:3000/orders', 
+      const response = await fetch('https://www.bortakvall.se/api/orders', 
       {
         method: 'POST',
         headers: {
